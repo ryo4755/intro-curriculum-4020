2fa0ba3e60d5b1d2dc9c50e50f7ead959027e577
ryo <ryo.shiina@azone-japan.co.jp>
2026-04-28 15:00:11 +0900

    練習提出

--- index.js
+++ index.js
@@ -20,3 +20,21 @@ function getC() {
 
 // TODO ここに getA, getB, getC で得られる結果をかけあわせた結果 2431 を標準出力するコードを記述する
 // ただし Promise チェイン(then関数の結果に対するthen関数の呼び出し)を一度は用いて実装をすること
+
+/*
+getA().then((val) => {
+  let ans = val;
+  getB().then((val) => {
+    ans = ans * val;
+    getC().then((val) => {
+      ans = ans * val;
+      console.log(`${ans}`);
+    });
+  });
+});
+*/
+getA().then(async vala => {
+  const valb = await getB();
+  const valc = await getC();
+  console.log(`${vala * valb * valc}`);
+});
